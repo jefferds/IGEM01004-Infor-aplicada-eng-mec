be972e88559b092b04eb3596f11222a63829e6c4
jefferds <jefferds@gmail.com>
2022-07-20 22:09:57 -0300

    20jul2022

---
+++ Unidade 01 - Introdução.jl
@@ -0,0 +1,66 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 01 - Introdução
+
+### Tema relacionado na ementa:
+
+1. Introdução e conceitos básicos
+2. Hardware
+3. Software
+
+### Resultados de aprendizagem esperados
+
+✔ Apresentar os conceitos de Hardware e Software.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 21/jul/2022)*,
+ 
+- _Introdução e conceitos básicos_;
+
+**Fonte:**  Pesquisa na internet e nos slides disponibilizados no Classroom.
+
+🕑 2. Estudar os slides disponibilizados e postar no Classroom resumo de até uma página do que estudou *(até 26/jul/2022)*;
+
+🕔 3. Seleção de temas com justificativa para o que será aprendendido e experienciado durante a unidade e cada estudante elabora os estudos dos temas propostos *(até 26/jul/2022)*;
+
+🕙 4. Entrega dos relatórios e apresentações dos temas pelos grupos *(até 28/jul/2028)*;
+
+🕑 5. Avaliações *(até 02/agos/2022)* e
+
+🕒 6. Feedbac *(até 02/out/2022)*.
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.3"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╠═64cb0563-2802-459e-8b14-2430546554ab
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
